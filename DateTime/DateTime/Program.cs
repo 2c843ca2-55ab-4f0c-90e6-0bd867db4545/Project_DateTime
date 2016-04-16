01e952d0a4818ce35c879148ce6839edd23152f0
2c843ca2-55ab-4f0c-90e6-0bd867db4545 <chenakeshava1@gmail.com>
2016-04-16 21:26:06 +0530

    Initial Commit

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateTimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZoneInfo tzByID = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzByID));
+            TimeZone zone = TimeZone.CurrentTimeZone;
+            DateTime dt = Convert.ToDateTime("10-12-2013 04:05:20");
+            Console.WriteLine(dt);
+            // Demonstrate ToLocalTime and ToUniversalTime.
+            Console.WriteLine(zone.StandardName);
+            DateTime local = zone.ToLocalTime(DateTime.Now);
+            DateTime universal = zone.ToUniversalTime(DateTime.Now);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(local);
+            Console.WriteLine(DateTime.UtcNow);
+            Console.WriteLine(universal);
+           // TimeZoneInfo tzByID = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            Console.WriteLine(tzByID.GetUtcOffset(dt));
+            TimeSpan offset = zone.GetUtcOffset(DateTime.Now);
+           
+            Console.WriteLine(offset);
+            Console.ReadLine();
+
+        }
+    }
+}
